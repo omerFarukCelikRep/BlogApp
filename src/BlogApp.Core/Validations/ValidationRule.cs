b569a4ad305923a9b9f652df0c50ffa5721bfac0
ofc <farukcelik2534@gmail.com>
2025-12-15 19:49:43 +0300

    validation rule condition added

--- src/BlogApp.Core/Validations/ValidationRule.cs
+++ src/BlogApp.Core/Validations/ValidationRule.cs
@@ -8,28 +8,21 @@ public class ValidationRule<T, TProperty>(string propertyName, Func<T, TProperty
     private readonly List<Func<TProperty, CancellationToken, Task<bool>>> _asyncRules = [];
     private readonly List<string> _asyncErrors = [];
 
+    private Func<T, bool>? _condition;
+
     public Func<T, TProperty> PropertyFunc { get; } = propertyFunc;
     public string PropertyName { get; } = propertyName;
 
-    public ValidationRule<T, TProperty> Must(Func<TProperty, bool> condition, string message)
-    {
-        _rules.Add(condition);
-        _errors.Add(message);
-        return this;
-    }
-
-    public ValidationRule<T, TProperty> MustAsync(Func<TProperty, CancellationToken, Task<bool>> condition, string message)
-    {
-        _asyncRules.Add(condition);
-        _asyncErrors.Add(message);
-        return this;
-    }
+    internal bool CanExecute(T instance) => _condition?.Invoke(instance) ?? true;
 
     internal async Task<IEnumerable<ValidationError>> ValidateAsync(T instance,
         CancellationToken cancellationToken = default)
     {
         var value = PropertyFunc(instance);
         List<ValidationError> validationErrors = [];
+
+        if (!CanExecute(instance))
+            return validationErrors;
 
         for (int i = 0; i < _rules.Count; i++)
         {
@@ -44,5 +37,26 @@ public class ValidationRule<T, TProperty>(string propertyName, Func<T, TProperty
         }
 
         return validationErrors;
+    }
+
+    public ValidationRule<T, TProperty> Must(Func<TProperty, bool> condition, string message)
+    {
+        _rules.Add(condition);
+        _errors.Add(message);
+        return this;
+    }
+
+    public ValidationRule<T, TProperty> MustAsync(Func<TProperty, CancellationToken, Task<bool>> condition,
+        string message)
+    {
+        _asyncRules.Add(condition);
+        _asyncErrors.Add(message);
+        return this;
+    }
+
+    public ValidationRule<T, TProperty> When(Func<T, bool> predicate)
+    {
+        _condition = predicate;
+        return this;
     }
 }
