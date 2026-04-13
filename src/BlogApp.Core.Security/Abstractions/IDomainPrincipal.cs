d043819a7939a52b42ab56be94610be957ec6862
ofc <farukcelik2534@gmail.com>
2026-04-14 00:58:44 +0300

    domain principal changes

--- src/BlogApp.Core.Security/Abstractions/IDomainPrincipal.cs
+++ src/BlogApp.Core.Security/Abstractions/IDomainPrincipal.cs
@@ -1,7 +1,19 @@
+using BlogApp.Core.Security.Constants;
+using BlogApp.Core.Security.Enums;
+
 namespace BlogApp.Core.Security.Abstractions;
 
 public interface IDomainPrincipal
 {
     public Guid UserId { get; }
+    public string? Username { get; }
+    public string? Email { get; }
     bool IsAuthenticated { get; }
+    IReadOnlyList<Role> Roles { get; }
+    IReadOnlyList<string> Permissions { get; }
+    
+    bool IsInRole(Role role);
+    bool HasPermission(string permission);
+    bool HasAnyPermission(params List<string> permissions);
+    bool HasAllPermissions(params List<string> permissions);
 }
--- src/BlogApp.Infrastructure.Security/Providers/DomainPrincipal.cs
+++ src/BlogApp.Infrastructure.Security/Providers/DomainPrincipal.cs
@@ -2,6 +2,7 @@ using System.ComponentModel;
 using System.Security.Claims;
 using BlogApp.Core.Security.Abstractions;
 using BlogApp.Core.Security.Constants;
+using BlogApp.Core.Security.Enums;
 using Microsoft.AspNetCore.Http;
 
 namespace BlogApp.Infrastructure.Security.Providers;
@@ -11,14 +12,17 @@ public class DomainPrincipal(IHttpContextAccessor contextAccessor) : IDomainPrin
     private readonly ClaimsPrincipal? _principal =
         contextAccessor.HttpContext?.User ?? Thread.CurrentPrincipal as ClaimsPrincipal;
 
-    public Guid UserId => GetClaim<Guid>(ClaimTypes.NameIdentifier);
+    public Guid UserId => Guid.TryParse(_principal?.FindFirstValue(ClaimTypes.NameIdentifier), out var id)
+        ? id 
+        : Guid.Empty;
+    public string? Username => GetClaim<string>(ClaimTypes.Name);
     public string FirstName => GetClaim<string>(ClaimTypes.GivenName)!;
     public string LastName => GetClaim<string>(ClaimTypes.Surname)!;
     public string FullName => $"{FirstName} {LastName}";
-    public string Email => GetClaim<string>(ClaimTypes.Email)!;
+    public string? Email => GetClaim<string>(ClaimTypes.Email)!;
     public bool IsAuthenticated => _principal?.Identity?.IsAuthenticated ?? false;
-    public IReadOnlyList<string> Roles => GetClaims(ClaimTypes.Role);
-    public IReadOnlyList<string> Permissions => GetClaims(CustomClaimTypes.Permission);
+    public IReadOnlyList<Role> Roles => GetClaims<Role>(CustomClaimTypes.Roles);
+    public IReadOnlyList<string> Permissions => GetClaims(CustomClaimTypes.Permissions);
 
     private T? GetClaim<T>(string claim)
     {
@@ -37,4 +41,26 @@ public class DomainPrincipal(IHttpContextAccessor contextAccessor) : IDomainPrin
 
         return claims.Select(c => c.Value).ToArray();
     }
+
+    private T[] GetClaims<T>(string claimType)
+    {
+        var claims = _principal?.FindAll(claimType);
+        if (_principal?.Identity?.IsAuthenticated is not true || claims is null)
+            return [];
+        
+        var converter = TypeDescriptor.GetConverter(typeof(T));
+        return claims.Select(x => x.Value)
+            .Select(x => (T?)converter.ConvertFrom(x))
+            .Where(x => x is not null)
+            .Select(x => x!)
+            .ToArray();
+    }
+
+    public bool IsInRole(Role role) => Roles.Contains(role);
+
+    public bool HasPermission(string permissions) => Permissions.Contains(permissions);
+
+    public bool HasAnyPermission(params List<string> permissions) => permissions.Any(HasPermission);
+
+    public bool HasAllPermissions(params List<string> permissions) => permissions.All(HasPermission);
 }
