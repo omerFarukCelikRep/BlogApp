36474832104f6ecda6aeb62412730fae88430ca0
ofc <@>
2026-06-03 00:15:12 +0300

    Application - behavior order changed

--- src/BlogApp.Application/Extensions/ServiceCollectionExtensions.cs
+++ src/BlogApp.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using BlogApp.Core.Caching.Behaviors;
 using BlogApp.Core.Logging.Behaviors;
 using BlogApp.Core.Mediator.Behaviors;
 using BlogApp.Core.Mediator.Extensions;
@@ -11,17 +12,20 @@ namespace BlogApp.Application.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    extension(IServiceCollection services)
     {
-        services.AddMediator(Assembly.GetExecutingAssembly())
-            .AddValidators(Assembly.GetExecutingAssembly())
-            .AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestHandlerPreProcesserBehavior<,>))
-            .AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestHandlerPostProcesserBehavior<,>))
-            .AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizeBehavior<,>))
-            .AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>))
-            .AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
-        // .AddTransient(typeof(IPipelineBehavior<,>), typeof(CachingBehavior<,>)); //TODO
+        public IServiceCollection AddApplicationServices()
+        {
+            services.AddMediator(Assembly.GetExecutingAssembly())
+                .AddValidators(Assembly.GetExecutingAssembly())
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>))
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestHandlerPreProcessorBehavior<,>))
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestHandlerPostProcessorBehavior<,>))
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehavior<,>))
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>))
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(CachingBehavior<,>));
 
-        return services;
+            return services;
+        }
     }
 }
