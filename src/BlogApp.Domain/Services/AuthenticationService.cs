d3d19c4b21293314e423d4d69fe41e697c71803c
ofc <@>
2026-06-03 00:13:15 +0300

    Domain - authentication service changes

--- src/BlogApp.Domain/Services/AuthenticationService.cs
+++ src/BlogApp.Domain/Services/AuthenticationService.cs
@@ -1,9 +1,14 @@
 using BlogApp.Core.Results;
 using BlogApp.Core.Security.Abstractions;
+using BlogApp.Core.Security.Models;
+using BlogApp.Core.Security.Options;
 using BlogApp.Core.Security.Utils;
 using BlogApp.Domain.Abstractions.Repositories;
 using BlogApp.Domain.Abstractions.Services;
+using BlogApp.Domain.Constants;
 using BlogApp.Domain.Models.Auth;
+using BlogApp.Domain.Options;
+using Microsoft.Extensions.Options;
 using Role = BlogApp.Core.Security.Enums.Role;
 
 namespace BlogApp.Domain.Services;
@@ -12,28 +17,55 @@ public class AuthenticationService(
     IUserRepository userRepository,
     IRoleRepository roleRepository,
     IJwtProvider jwtProvider,
-    IRefreshTokenProvider refreshTokenProvider)
+    IRefreshTokenProvider refreshTokenProvider,
+    IOptions<LoginOptions> loginOptions,
+    IOptions<JwtOptions> jwtOptions)
     : IAuthenticationService
 {
+    private readonly LoginOptions _loginOptions = loginOptions.Value;
+    private readonly JwtOptions _jwtOptions = jwtOptions.Value;
+
     public async Task<Result<LoginResult>> LoginAsync(LoginArgs args, CancellationToken cancellationToken = default)
     {
         var user = await userRepository.GetAsync(x => x.Email.Equals(args.Email, StringComparison.OrdinalIgnoreCase),
             tracking: true, cancellationToken);
         if (user is null)
-            return Result<LoginResult>.Failed(null, string.Empty, 401);
+            return Result<LoginResult>.Failed(401, Errors.Auth.LoginFailed);
+
+        if (user.IsLockedOut())
+            return Result<LoginResult>.Failed(401, Errors.Auth.AccountLocked);
 
         var passwordVerified = PasswordHasher.VerifyPassword(args.Password, user.Password);
         if (!passwordVerified)
-            return Result<LoginResult>.Failed(null, string.Empty, 401);
-
-        var token = await jwtProvider.GenerateTokenAsync(user.Id, cancellationToken);
-        var refreshToken = await refreshTokenProvider.GenerateRefreshTokenAsync(user.Id, cancellationToken);
-        LoginResult result = new()
         {
-            Token = token,
-            RefreshToken = refreshToken
+            user.AccessFailedCount++;
+            if (user.AccessFailedCount >= _loginOptions.FailLimit)
+                user.LockoutEnd = DateTimeOffset.Now.AddMinutes(_loginOptions.FailLimit);
+
+            await userRepository.SaveChangesAsync(cancellationToken);
+            return Result<LoginResult>.Failed(401, Errors.Auth.InvalidCredentials);
+        }
+
+        user.AccessFailedCount = 0;
+        user.LockoutEnd = null;
+
+        var tokenArgs = new TokenArgs()
+        {
+            UserId = user.Id,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Email = user.Email,
+            Username = user.Username,
+            Roles = [..user.UserRoles.Select(x => x.Role!.Name)],
+            Permissions =
+                [..user.UserRoles.SelectMany(x => x.Role!.RolePermissions.Select(p => p.Permission!.ToString()))]
         };
-        return Result<LoginResult>.Success(result, "Success", 200); //TODO: Resource magic string
+        var token = await jwtProvider.GenerateTokenAsync(tokenArgs, cancellationToken);
+        var refreshToken = await refreshTokenProvider.GenerateAsync(user.Id, cancellationToken);
+
+        await userRepository.SaveChangesAsync(cancellationToken);
+        LoginResult result = new(token, refreshToken, DateTimeOffset.Now.AddMinutes(_jwtOptions.ExpirationMinutes));
+        return Result<LoginResult>.Success(result);
     }
 
     public async Task<Result> RegisterAsync(RegisterArgs args, CancellationToken cancellationToken = default)
@@ -42,12 +74,15 @@ public class AuthenticationService(
             await userRepository.AnyAsync(x => x.Email.ToLower().Equals(args.Email.ToLower()),
                 cancellationToken);
         if (userExist)
-            return Result.Failed("Email already in user", 400); //TODO : Resource Magic string
+            return Result.Failed(Errors.Auth.EmailAlreadyExist, 400);
 
-        var hashedPassword = PasswordHasher.HashPassword(args.Password);
         var role = await roleRepository.GetAsync(
             x => x.Name.Equals(nameof(Role.Author)), tracking: false,
             cancellationToken);
+        if (role is null)
+            return Result.Failed(Errors.Role.NotFound, 400);
+
+        var hashedPassword = PasswordHasher.HashPassword(args.Password);
         User user = new()
         {
             FirstName = args.FirstName,
@@ -57,15 +92,15 @@ public class AuthenticationService(
             Password = hashedPassword,
             EmailConfirmed = false
         };
-        user.Roles.Add(new()
+        user.UserRoles.Add(new()
         {
             User = user,
-            RoleId = role!.Id
+            RoleId = role.Id
         });
 
         await userRepository.AddAsync(user, cancellationToken);
         await userRepository.SaveChangesAsync(cancellationToken);
 
-        return Result.Success("Registered successfully", 200); //TODO:Resource Magic string 
+        return Result.Success();
     }
 }
